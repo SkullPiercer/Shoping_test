12d3dcfb1597c997d5c32027bb68c0925c34b0c4
Alexander <akatsuki_sanya@vk.com>
2024-12-11 22:57:31 +0500

    Made Post for Product superuserOnly

--- app/api/endpoints/product.py
+++ app/api/endpoints/product.py
@@ -4,8 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.validators import check_name_duplicate
 from app.core.db import get_async_session
+from app.models import Product
 from app.crud.product import product_crud
 from app.schemas.product import ProductCreate, ProductDB
+from app.core.user import current_superuser
 
 router = APIRouter()
 
@@ -14,10 +16,23 @@ router = APIRouter()
     '/',
     response_model=ProductDB,
     response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def create_new_product(
         product: ProductCreate,
         session: AsyncSession = Depends(get_async_session),
 ):
+    """SuperUser only."""
     await check_name_duplicate(product.name, session)
     return await product_crud.create(product, session)
+
+
+@router.get(
+    '/',
+    response_model=list[ProductDB],
+    response_model_exclude_none=True,
+)
+async def get_all_product(
+        session: AsyncSession = Depends(get_async_session),
+) -> list[Product]:
+    return await product_crud.get_multi(session=session)
