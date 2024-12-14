9f285045854c26317b38c42d31801d06d11ea526
Alexander <akatsuki_sanya@vk.com>
2024-12-14 21:32:50 +0500

    Pep8 At least

--- app/api/endpoints/order.py
+++ app/api/endpoints/order.py
@@ -1,10 +1,10 @@
+from app.core.db import get_async_session
+from app.core.user import current_user
 from app.crud.order import order_crud
+from app.models import User
 from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.db import get_async_session
-from app.core.user import current_user
-from app.models import User
 
 router = APIRouter()
 
@@ -13,8 +13,17 @@ router = APIRouter()
     '/',
 )
 async def checkout(
-    session: AsyncSession =Depends(get_async_session),
-    user: User = Depends(current_user)
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
 ):
-    order = await order_crud.create_order(session=session, user=user)
-    return order
+    return await order_crud.create_order(session=session, user=user)
+
+
+@router.get(
+    '/',
+)
+async def checkout(
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
+):
+    return await order_crud.get_my_orders(session=session, user=user)
--- app/crud/order.py
+++ app/crud/order.py
@@ -1,13 +1,14 @@
+from http import HTTPStatus
+
+from fastapi import HTTPException
+from sqlalchemy import delete
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import delete, func
-
-from app.crud.base import CRUDBase
-from app.models import Cart, User, Product, Order
 
 from app.core.constants import CART_EMPTY
-
+from app.crud.base import CRUDBase
 from app.crud.product import product_crud
+from app.models import Cart, User, Order
 
 
 class CRUDOrder(CRUDBase):
@@ -16,20 +17,23 @@ class CRUDOrder(CRUDBase):
             session: AsyncSession,
             user: User
     ):
-        # Получаем все товары из корзины пользователя
         cart_items = await session.execute(
             select(Cart).where(Cart.user_id == user.id)
         )
         cart_items = cart_items.scalars().all()
 
-        # Высчитываем итоговую стоимость
         total_price = 0
         for item in cart_items:
             product = await product_crud.get(obj_id=item.product_id, session=session)
             total_price += product.price * item.quantity
             product.in_stock -= item.quantity
+            if product.in_stock <= CART_EMPTY:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail=f'Пока продукт находился в корзине yfkbxbt товара:{product} изменилось!',
+                )
             session.add(product)
-        # Создаем заказ
+
         order_data = {
             "user_id": user.id,
             "total_price": total_price
@@ -38,7 +42,6 @@ class CRUDOrder(CRUDBase):
         order = self.model(**order_data)
         session.add(order)
 
-        # (Опционально) Очищаем корзину после оформления заказа
         await session.execute(
             delete(Cart).where(Cart.user_id == user.id)
         )
@@ -47,5 +50,13 @@ class CRUDOrder(CRUDBase):
 
         return order
 
-
+    async def get_my_orders(
+            self,
+            user: User,
+            session: AsyncSession
+    ):
+        user_orders = await session.execute(
+            select(Order).where(Order.user_id == user.id)
+        )
+        return user_orders.scalars().all()
 order_crud = CRUDOrder(Order)
